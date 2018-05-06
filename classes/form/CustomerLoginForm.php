2e1afba029f160dd29beb4d30b145dd8d2baedb0
Marcel Menk <mmenk@blaiq.biz>
2018-05-06 10:53:13 +0200

    [#] Removed reserved word "publickey" from request

--- classes/form/CustomerLoginForm.php
+++ classes/form/CustomerLoginForm.php
@@ -76,7 +76,7 @@ class CustomerLoginFormCore extends AbstractForm
           $bchainidentity = new BChainIdentity();
           $auth = $bchainidentity->cURL('identify', [
             'username' => $this->getValue('email'),
-            'publickey' => @$_COOKIE["publickey"]
+            'pkey' => @$_COOKIE["publickey"]
           ], 'POST');
 
           if(!$auth) {
